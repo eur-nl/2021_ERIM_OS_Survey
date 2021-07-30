cd60a76152866e16617ec7d086285fb187fccb07
Antonio Schettino <antonio.schettino@igdore.org>
2021-07-30 19:19:56 +0200

    plot background: white instead of transparent

--- code/theme_custom.R
+++ code/theme_custom.R
@@ -2,6 +2,7 @@ theme_custom <-
   ggplot2::theme_void(base_size = 12) +
   ggplot2::theme(
     plot.title = element_text(size = 22, hjust = .5),
+    plot.background = element_rect(fill = "white", color = "transparent"),
     legend.box.background = element_rect(color = "transparent"),
     legend.title = element_blank(),
     legend.text = element_text(size = 12),
