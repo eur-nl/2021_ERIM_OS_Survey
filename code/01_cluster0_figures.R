b60eaad8782570356efd66068d067c53132bec70
Antonio Schettino <antonio.schettino@igdore.org>
2021-07-30 18:14:29 +0200

    cluster 0: redraw donut plots using ggplot2 and ggrepel

--- code/01_cluster0_figures.R
+++ code/01_cluster0_figures.R
@@ -8,30 +8,17 @@ set.seed(seed_synth)
 
 # install.packages("here")
 # install.packages("tidyverse")
-# install.packages("ggpubr")
+# install.packages("ggrepel")
 # install.packages("viridis")
 
 # Load packages --------------------------------------------------------
 
 library(here)
 library(tidyverse)
-library(ggpubr)
+library(ggrepel)
 library(viridis)
 
-# Custom ggplot theme ----------------------------------------------------------------
-
-theme_custom <-
-  theme_pubr(base_size = 12) +
-  theme(
-    strip.text = element_text(
-      hjust = .5,
-      size = 20
-    ),
-    plot.title = element_text(size = 22, hjust = .5),
-    legend.box.background = element_rect(color = "transparent"),
-    legend.title = element_blank(),
-    legend.position = "bottom"
-  )
+source(here("code", "theme_custom.R")) # custom ggplot2 theme
 
 # Data ----------------------------------------------------------------
 
@@ -71,13 +58,23 @@ cluster <-
   ungroup() %>%
   group_by(question) %>%
   mutate(
-    perc = round(number_responses / sum(number_responses) * 100, 2), # calculate percentage
-    lab_perc = paste(perc, "%", sep = "") # percentage as text (for labels)
+    prop = number_responses / sum(number_responses), # calculate proportion
+    perc = round(prop * 100, 2), # calculate percentage
+    ymax = cumsum(prop), # top of each rectangle
+    ymin = c(0, head(ymax, n = -1)), # bottom of each rectangle
+    lab_pos = (ymax + ymin) / 2, # label position
+    lab_perc = paste(perc, "%", sep = ""), # percentage as text (for labels)
   ) %>%
   ungroup()
 
 # extract questions
 questions <- levels(cluster$question)
+
+# save for final report
+write_csv(
+  cluster,
+  here("data", "preproc", "cluster0.csv")
+)
 
 # Question 1 ----------------------------------------------------------------
 
@@ -88,17 +85,16 @@ data_cluster0_question1 <-
   dplyr::select(-number_responses)
 
 donut_cluster0_question1 <-
-  data_cluster0_question1 %>%
-  ggdonutchart(
-    "perc",
-    label = "lab_perc",
-    lab.pos = "out",
-    color = "black",
-    fill = "item",
-    palette = plasma(length(unique(.$item))),
-    ggtheme = theme_custom
-  ) +
-  ggtitle(questions[1])
+  data_cluster0_question1 %>% 
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black") +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle(questions[1]) +
+  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[1])$item)), byrow = TRUE)) +
+  theme_custom
 
 donut_cluster0_question1
 
@@ -124,18 +120,16 @@ data_cluster0_question2 <-
   dplyr::select(-number_responses)
 
 donut_cluster0_question2 <-
-  data_cluster0_question2 %>%
-  ggdonutchart(
-    "perc",
-    label = "lab_perc",
-    lab.pos = "out",
-    color = "black",
-    fill = "item",
-    palette = plasma(length(unique(.$item))),
-    ggtheme = theme_custom
-  ) +
+  data_cluster0_question2 %>% 
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black") +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
   ggtitle(questions[2]) +
-  guides(fill = guide_legend(nrow = length(unique(data_cluster0_question2$item)), byrow = TRUE))
+  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[2])$item)), byrow = TRUE)) +
+  theme_custom
 
 donut_cluster0_question2
 
@@ -162,16 +156,14 @@ data_cluster0_question3 <-
 
 donut_cluster0_question3 <-
   data_cluster0_question3 %>%
-  ggdonutchart(
-    "perc",
-    label = "lab_perc",
-    lab.pos = "out",
-    color = "black",
-    fill = "item",
-    palette = plasma(length(unique(.$item))),
-    ggtheme = theme_custom
-  ) +
-  ggtitle(questions[3])
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black") +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle(questions[3]) +
+  theme_custom
 
 donut_cluster0_question3
 
@@ -209,16 +201,14 @@ data_cluster0_question4 <-
 
 donut_cluster0_question4 <-
   data_cluster0_question4 %>%
-  ggdonutchart(
-    "perc",
-    label = "lab_perc",
-    lab.pos = "out",
-    color = "black",
-    fill = "item",
-    palette = plasma(length(unique(.$item))),
-    ggtheme = theme_custom
-  ) +
-  ggtitle(questions[4])
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black") +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle(questions[4]) +
+  theme_custom
 
 donut_cluster0_question4
 
@@ -256,16 +246,14 @@ data_cluster0_question5 <-
 
 donut_cluster0_question5 <-
   data_cluster0_question5 %>%
-  ggdonutchart(
-    "perc",
-    label = "lab_perc",
-    lab.pos = "out",
-    color = "black",
-    fill = "item",
-    palette = plasma(length(unique(.$item))),
-    ggtheme = theme_custom
-  ) +
-  ggtitle("Are you member of any research institute\naffiliated with RSM or ESE?") # title is too long, must be manually split into two lines
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black") +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle("Are you member of any research institute\naffiliated with RSM or ESE?") + # title is too long, must be manually split into two lines
+  theme_custom
 
 donut_cluster0_question5
 
