afa0b90d587d075b947a618150a7a9955a774a9f
Antonio Schettino <antonio.schettino@igdore.org>
2021-07-30 18:58:12 +0200

    cluster 1, all plots

--- code/02_cluster1_figures.R
+++ code/02_cluster1_figures.R
@@ -8,30 +8,17 @@ set.seed(seed_synth)
 
 # install.packages("here")
 # install.packages("tidyverse")
-# install.packages("ggpubr")
+# install.packages("ggrepel")
 # install.packages("viridis")
 
 # Load packages --------------------------------------------------------
 
 library(here)
 library(tidyverse)
-library(ggpubr)
+library(ggrepel)
 library(viridis)
 
-# Custom ggplot theme ----------------------------------------------------------------
-
-theme_custom <-
-  theme_pubr(base_size = 12) +
-  theme(
-    strip.text = element_text(
-      hjust = .5,
-      size = 20
-    ),
-    plot.title = element_text(size = 22, hjust = .5),
-    legend.box.background = element_rect(color = "transparent"),
-    legend.title = element_blank(),
-    legend.position = "bottom"
-  )
+source(here("code", "theme_custom.R")) # custom ggplot2 theme
 
 # Data ----------------------------------------------------------------
 
@@ -68,13 +55,23 @@ cluster <-
   ungroup() %>%
   group_by(question) %>%
   mutate(
-    perc = round(number_responses / sum(number_responses) * 100, 2), # calculate percentage
-    lab_perc = paste(perc, "%", sep = "") # percentage as text (for labels)
+    prop = number_responses / sum(number_responses), # calculate proportion
+    perc = round(prop * 100, 2), # calculate percentage
+    ymax = cumsum(prop), # top of each rectangle
+    ymin = c(0, head(ymax, n = -1)), # bottom of each rectangle
+    lab_pos = (ymax + ymin) / 2, # label position
+    lab_perc = paste(perc, "%", sep = ""), # percentage as text (for labels)
   ) %>%
   ungroup()
 
 # extract questions
 questions <- levels(cluster$question)
+
+# save for final report
+write_csv(
+  cluster,
+  here("data", "preproc", "cluster1.csv")
+)
 
 # Question 1 ----------------------------------------------------------------
 
@@ -97,18 +94,16 @@ data_cluster1_question1 <-
   ))
 
 donut_cluster1_question1 <-
-  data_cluster1_question1 %>%
-  ggdonutchart(
-    "perc",
-    label = "lab_perc",
-    lab.pos = "out",
-    color = "black",
-    fill = "item",
-    palette = plasma(length(unique(.$item))),
-    ggtheme = theme_custom
-  ) +
-  ggtitle(questions[1]) +
-  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[1])$item)), byrow = TRUE))
+  data_cluster1_question1 %>% 
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black", box.padding = 0.5) +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle(str_wrap(questions[1], width = 40)) + # if title is too long, split into two lines with specified max width
+  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[1])$item)), byrow = TRUE)) +
+  theme_custom
 
 donut_cluster1_question1
 
@@ -151,18 +146,16 @@ data_cluster1_question2 <-
   ))
 
 donut_cluster1_question2 <-
-  data_cluster1_question2 %>%
-  ggdonutchart(
-    "perc",
-    label = "lab_perc",
-    lab.pos = "out",
-    color = "black",
-    fill = "item",
-    palette = plasma(length(unique(.$item))),
-    ggtheme = theme_custom
-  ) +
-  ggtitle("Are you sharing your knowledge about\nopen science practices with others?") + # title is too long, must be manually split into two lines
-  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[2])$item)), byrow = TRUE))
+  data_cluster1_question2 %>% 
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black", box.padding = 0.5) +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle(str_wrap(questions[2], width = 40)) +
+  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[2])$item)), byrow = TRUE)) +
+  theme_custom
 
 donut_cluster1_question2
 
@@ -202,17 +195,15 @@ data_cluster1_question3 <-
 
 donut_cluster1_question3 <-
   data_cluster1_question3 %>%
-  ggdonutchart(
-    "perc",
-    label = "lab_perc",
-    lab.pos = "out",
-    color = "black",
-    fill = "item",
-    palette = plasma(length(unique(.$item))),
-    ggtheme = theme_custom
-  ) +
-  ggtitle("During March 2021 ERIM launched an ORCID campaign.\nDid you participate in it and got your own ORCID iD?") + # title is too long, must be manually split into two lines
-  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[3])$item)), byrow = TRUE))
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black", box.padding = 0.5) +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle(str_wrap(questions[3], width = 40)) +
+  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[3])$item)), byrow = TRUE)) +
+  theme_custom
 
 donut_cluster1_question3
 
