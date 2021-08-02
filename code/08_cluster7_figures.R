8c636810aaed9bcca6d4de307538afde8d206a1f
Antonio Schettino <antonio.schettino@igdore.org>
2021-08-02 10:46:55 +0200

    complete cluster 7
    image is slightly larger (10 x 10 instead of 8 x 8 cm) to accommodate the large number of labels

--- code/08_cluster7_figures.R
+++ code/08_cluster7_figures.R
@@ -8,30 +8,19 @@ set.seed(seed_synth)
 
 # install.packages("here")
 # install.packages("tidyverse")
-# install.packages("ggpubr")
+# install.packages("ggrepel")
 # install.packages("viridis")
 
 # Load packages --------------------------------------------------------
 
 library(here)
 library(tidyverse)
-library(ggpubr)
+library(ggrepel)
 library(viridis)
 
-# Custom ggplot theme ----------------------------------------------------------------
+source(here("code", "theme_custom.R")) # custom ggplot2 theme
 
-theme_custom <-
-  theme_pubr(base_size = 12) +
-  theme(
-    strip.text = element_text(
-      hjust = .5,
-      size = 20
-    ),
-    plot.title = element_text(size = 22, hjust = .5),
-    legend.box.background = element_rect(color = "transparent"),
-    legend.title = element_blank(),
-    legend.position = "bottom"
-  )
+options(ggrepel.max.overlaps = Inf) # always show all labels, regardless of overlaps
 
 # Data ----------------------------------------------------------------
 
@@ -58,17 +47,82 @@ cluster <-
   group_by(question, item) %>%
   summarize(number_responses = n()) %>%
   ungroup() %>%
+  group_by(question) %>%
   mutate(
-    perc = round(number_responses / sum(number_responses) * 100, 2), # calculate percentage
-    lab_perc = paste(perc, "%", sep = "") # percentage as text (for labels)
+    prop = number_responses / sum(number_responses), # calculate proportion
+    perc = round(prop * 100, 2), # calculate percentage
+    ymax = cumsum(prop), # top of each rectangle
+    ymin = c(0, head(ymax, n = -1)), # bottom of each rectangle
+    lab_pos = (ymax + ymin) / 2, # label position
+    lab_perc = paste(perc, "%", sep = ""), # percentage as text (for labels)
   ) %>%
   ungroup()
 
 # extract questions
 questions <- levels(cluster$question)
 
+# save for final report
+write_csv(
+  cluster,
+  here("data", "preproc", "cluster7.csv")
+)
+
 # Question 1 ----------------------------------------------------------------
 
+data_cluster7_question1 <-
+  cluster %>%
+  filter(question == questions[1]) %>%
+  droplevels() %>%
+  dplyr::select(-number_responses) %>%
+  # reorder responses
+  mutate(item = factor(
+    item,
+    levels = c(
+      "Lack of supporting infrastructure (e.g., open data platforms)",
+      "Lack of training required to implement open science practices",
+      "Lack of professional staff that provide support for open science practices",
+      "Lack of credit in my institution for engaging in open science",
+      "Lack of recognition in my field about the value of open science practices",
+      "Lack of interest from researchers",
+      "Lack of funding for open access publishing",
+      "Lack of research funding to support open science practices",
+      "Lack of mandates from funders",
+      "Lack of information about open science practices",
+      "Lack of time to engage in open science practices",
+      "Lack of time to learn open science practices",
+      "I do not perceive any barriers",
+      "Other",
+      "I don’t know/prefer not to answer"
+    ),
+    ordered = TRUE
+  ))
 
+donut_cluster7_question1 <-
+  data_cluster7_question1 %>%
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black", box.padding = 0.5) +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle(str_wrap(questions[1], width = 40)) + # if title is too long, split into two lines with specified max width
+  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[1])$item)), byrow = TRUE)) +
+  theme_custom + 
+  theme(legend.position = "right")
 
+donut_cluster7_question1
 
+# save to file
+ggsave(
+  filename = "donut_cluster7_question1.png",
+  plot = donut_cluster7_question1,
+  device = "png",
+  path = here("img"),
+  scale = 3,
+  width = 10,
+  height = 10,
+  units = "cm",
+  dpi = 96
+)
+
+# END ----------------------------------------------------------------
