2db76cc41ada39c14790458986cf38dc99fe6a55
Antonio Schettino <antonio.schettino@igdore.org>
2021-08-02 09:48:00 +0200

    complete cluster 5
    legend in question 3 is too big to be put below the graph, so now it's on the right

--- code/06_cluster5_figures.R
+++ code/06_cluster5_figures.R
@@ -8,30 +8,19 @@ set.seed(seed_synth)
 
 # install.packages("here")
 # install.packages("tidyverse")
-# install.packages("ggpubr")
+# install.packages("ggrepel")
 # install.packages("viridis")
 
 # Load packages --------------------------------------------------------
 
 library(here)
 library(tidyverse)
-library(ggpubr)
+library(ggrepel)
 library(viridis)
 
-# Custom ggplot theme ----------------------------------------------------------------
+source(here("code", "theme_custom.R")) # custom ggplot2 theme
 
-theme_custom <-
-  theme_pubr(base_size = 12) +
-  theme(
-    strip.text = element_text(
-      hjust = .5,
-      size = 20
-    ),
-    plot.title = element_text(size = 22, hjust = .5),
-    legend.box.background = element_rect(color = "transparent"),
-    legend.title = element_blank(),
-    legend.position = "bottom"
-  )
+options(ggrepel.max.overlaps = Inf) # always show all labels, regardless of overlaps
 
 # Data ----------------------------------------------------------------
 
@@ -68,16 +57,167 @@ cluster <-
   ungroup() %>%
   group_by(question) %>%
   mutate(
-    perc = round(number_responses / sum(number_responses) * 100, 2), # calculate percentage
-    lab_perc = paste(perc, "%", sep = "") # percentage as text (for labels)
+    prop = number_responses / sum(number_responses), # calculate proportion
+    perc = round(prop * 100, 2), # calculate percentage
+    ymax = cumsum(prop), # top of each rectangle
+    ymin = c(0, head(ymax, n = -1)), # bottom of each rectangle
+    lab_pos = (ymax + ymin) / 2, # label position
+    lab_perc = paste(perc, "%", sep = ""), # percentage as text (for labels)
   ) %>%
   ungroup()
 
 # extract questions
 questions <- levels(cluster$question)
 
+# save for final report
+write_csv(
+  cluster,
+  here("data", "preproc", "cluster5.csv")
+)
+
 # Question 1 ----------------------------------------------------------------
 
+data_cluster5_question1 <-
+  cluster %>%
+  filter(question == questions[1]) %>%
+  droplevels() %>%
+  dplyr::select(-number_responses) %>%
+  # reorder responses
+  mutate(item = factor(
+    item,
+    levels = c(
+      "Extremely important",
+      "Very important",
+      "Moderately important",
+      "Slightly important",
+      "Not at all important",
+      "I don’t know/prefer not to answer"
+    ),
+    ordered = TRUE
+  ))
 
+donut_cluster5_question1 <-
+  data_cluster5_question1 %>%
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black", box.padding = 0.5) +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle(str_wrap(questions[1], width = 40)) + # if title is too long, split into two lines with specified max width
+  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[1])$item)), byrow = TRUE)) +
+  theme_custom
 
+donut_cluster5_question1
 
+# save to file
+ggsave(
+  filename = "donut_cluster5_question1.png",
+  plot = donut_cluster5_question1,
+  device = "png",
+  path = here("img"),
+  scale = 3,
+  width = 8,
+  height = 8,
+  units = "cm",
+  dpi = 96
+)
+
+# Question 2 ----------------------------------------------------------------
+
+data_cluster5_question2 <-
+  cluster %>%
+  filter(question == questions[2]) %>%
+  droplevels() %>%
+  dplyr::select(-number_responses) %>%
+  # reorder responses
+  mutate(item = factor(
+    item,
+    levels = c(
+      "Until now, I was unaware of \"pre-publication archiving\"",
+      "I am aware of pre-publication archiving but have not used it",
+      "I have some experience with pre-publication archiving",
+      "I have extensive experience with preprint archiving",
+      "I don’t know/prefer not to answer"
+    ),
+    ordered = TRUE
+  ))
+
+donut_cluster5_question2 <-
+  data_cluster5_question2 %>%
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black", box.padding = 0.5) +
+  scale_fill_viridis_d(option = "plasma") +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle(str_wrap(questions[2], width = 40)) +
+  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[2])$item)), byrow = TRUE)) +
+  theme_custom
+
+donut_cluster5_question2
+
+# save to file
+ggsave(
+  filename = "donut_cluster5_question2.png",
+  plot = donut_cluster5_question2,
+  device = "png",
+  path = here("img"),
+  scale = 3,
+  width = 8,
+  height = 8,
+  units = "cm",
+  dpi = 96
+)
+
+# Question 3 ----------------------------------------------------------------
+
+data_cluster5_question3 <-
+  cluster %>%
+  filter(question == questions[3]) %>%
+  droplevels() %>%
+  dplyr::select(-number_responses) %>%
+  # reorder responses
+  mutate(item = factor(
+    item,
+    levels = c(
+      "Non-peer-reviewed findings might add noise to the literature",
+      "Some journals might not publish findings that are uploaded to a pre-publication archive",
+      "Making my work available pre-publication might reduce the number of citations to the ultimately published work",
+      "Other people might copy my research and publish it before I do",
+      "Availability of the pre-publication manuscript might highlight differences (e.g., errors in analysis, revisions to hypotheses) between the original conception of the research and the ultimately published work",
+      "I do not share any of these concerns",
+      "Other"
+    ),
+    ordered = TRUE
+  ))
+
+donut_cluster5_question3 <-
+  data_cluster5_question3 %>%
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = item)) +
+  geom_rect() +
+  geom_label_repel(x = 3.5, aes(y = lab_pos, label = lab_perc, color = "white"), size = 6, fill = "white", color = "black", box.padding = 0.5) +
+  scale_fill_viridis_d(option = "plasma", labels = function(x) str_wrap(x, width = 20)) +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  ggtitle(str_wrap(questions[3], width = 40)) +
+  guides(fill = guide_legend(nrow = length(unique(filter(cluster, question == questions[3])$item)), byrow = TRUE)) +
+  theme_custom +
+  theme(legend.position = "right")
+
+donut_cluster5_question3
+
+# save to file
+ggsave(
+  filename = "donut_cluster5_question3.png",
+  plot = donut_cluster5_question3,
+  device = "png",
+  path = here("img"),
+  scale = 3,
+  width = 8,
+  height = 8,
+  units = "cm",
+  dpi = 96
+)
+
+# END ----------------------------------------------------------------
